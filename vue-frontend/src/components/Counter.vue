d7ade797c0059ecf524d22058887b82f6073f417
Aria Rens <57843464+rens000@users.noreply.github.com>
2021-09-03 11:35:28 -0400

    Finish part #3 of frontend

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,5 +1,10 @@
 <template>
+<div>
   <p>I'm a Counter!</p>
+  <p>The current count is:  {{ count }} </p>
+  <button v-on:click="count += 1">Add 1</button>
+  <button v-on:click="count -= 1">Delete 1</button>
+</div>
 </template>
 
 <script>
